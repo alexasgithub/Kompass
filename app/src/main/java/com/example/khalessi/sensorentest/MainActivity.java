817245318dc345caba0b9932c790d6af4f56fde6
alexasgithub <alexandra.filbert@googlemail.com>
2018-01-19 14:25:31 +0100

    Compass implementation final

--- app/src/main/java/com/example/khalessi/sensorentest/MainActivity.java
+++ app/src/main/java/com/example/khalessi/sensorentest/MainActivity.java
@@ -6,15 +6,14 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -23,7 +22,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor mAccelerometer;
     private Sensor mMagnetometer;
-    private Sensor mProximity;
 
     private TextView tvSensorXVal, tvSensorYVal, tvSensorZVal;
     private TextView compassAngle;
@@ -41,9 +39,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private ImageView mPointer;
 
-  //  private float currentDegree = 0f;
-    public static String TAG = "1234";
-
+    /**
+     * Die einzelnen Views werden geholt und
+     * der SensorManager initializiert.
+     * Sensoren werden ausgelesen.
+     * Falls kein Magnetsensor im Smarphone vorhanden ist,
+     * erfolgt eine Meldung an den User.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,63 +70,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-     /*   if (mMagnetometer != null) {
-            sensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
-        }*/
+      //Test, ob Sensor.TYPE_MAGNETIC_FIELD existiert, sonst eine Meldung an den User
+       if (mMagnetometer == null) {
+           Toast.makeText(getApplicationContext(), "Sie haben keinen Sensor.TYPE_MAGNETIC_FIELD", Toast.LENGTH_LONG).show();
+
+        }
 
 
     }
 
+    /**
+     * Beim (wiederholten) Starten werden Listener für die Sensoren registriert.
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        // register this class as a listener for the orientation and
-        // accelerometer sensors
 
         sensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
         sensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
 
+    }
 
-     /*   mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, mAccelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL);
-
-        mMagnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR);
-        sensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-// Create listener
-        SensorEventListener proximitySensorListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                // More code goes here
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-            }
-        };
-
-        mProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        if(mProximity == null) {
-            Log.e(TAG, "Proximity sensor not available.");
-           
-        }
-        // Register it, specifying the polling interval in
-// microseconds
-        sensorManager.registerListener(proximitySensorListener,
-                mProximity, 2 * 1000 * 1000);
-*/
+    /**
+     * Beim Pausieren werden die Listener für die Sensoren entfernt.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, mAccelerometer);
+        sensorManager.unregisterListener(this, mMagnetometer);
 
     }
 
 
-
-
     /**
+     * Liest die Daten des Accelorometers aus und
+     * zeigt sie in den Textviews an.
+     * Überprüft die Beschleunigung und falls der Schwellwert von 2
+     * überschritten wird, erfolgt die Änderung der Hintergrundfarbe
      *
      * @param event
      */
-    private void getAccelerometer(SensorEvent event) {
+    private void getAccelerometerData(SensorEvent event) {
         float[] values = event.values;
         // Movement
         float x = Math.round(values[0]);
@@ -130,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float z = Math.round(values[2]);
 
 
-        tvSensorXVal.setText("X: " + x);
+        tvSensorXVal.setText(" X: " + x);
         tvSensorYVal.setText(" / Y: " + y);
         tvSensorZVal.setText(" / Z: " + z);
         ll_basic.setBackgroundResource(R.drawable.europa);
@@ -138,15 +127,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float accelationSquareRoot = (x * x + y * y + z * z)
                 / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
         long actualTime = event.timestamp;
-        if (accelationSquareRoot >= 2) //
+        if (accelationSquareRoot >= 2)
         {
-            if (actualTime - lastUpdate < 200) {
+            if (actualTime - lastUpdate < 2000) {
                 return;
             }
             lastUpdate = actualTime;
 
             if (color) {
-                ll_basic.setBackgroundColor(Color.WHITE);
+                ll_basic.setBackgroundResource(R.drawable.europa);
             } else {
                 ll_basic.setBackgroundColor(Color.RED);
             }
@@ -155,42 +144,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //sensorManager.unregisterListener(this);
-        sensorManager.unregisterListener(this, mAccelerometer);
-        sensorManager.unregisterListener(this, mMagnetometer);
 
-      //  sensorManager.unregisterListener(proximitySensorListener);
-    }
-
-
+    /**
+     * Notwendig zur Implementierung des Interfaces SensorEventListener
+     * @param sensor
+     * @param i
+     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-
+        //TODO Reaktion auf Änderung der Genauigkeit des Sensors
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_compass, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        //     if (id == R.id.action_settings) {
-        //        return true;
-        //   }
-        return super.onOptionsItemSelected(item);
-    }
-
+    /**
+     * Prüft welcher Sensor die Daten liefert und verzweigt entsprechend.
+     * Startet die Rotationsanimation für den Kompass.
+     * @param event
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
 
@@ -198,18 +167,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor == mAccelerometer) {
             System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
             mLastAccelerometerSet = true;
-            getAccelerometer(event);
+            getAccelerometerData(event);
         } else if (event.sensor == mMagnetometer) {
             System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
             mLastMagnetometerSet = true;
-          //  getCompass(event);
-      /*      float xv = Math.round(event.values[0]);
-            float yv = Math.round(event.values[1]);
-            float zv = Math.round(event.values[2]);
-            float degree = Math.round(xv);
 
-            compassAngle.setText("Ausrichtung X: " + Float.toString(xv) + " Y:" + Float.toString(yv) + " Z: " + Float.toString(zv));
-*/
 
         }
         if (mLastAccelerometerSet && mLastMagnetometerSet) {
@@ -220,6 +182,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             SensorManager.getOrientation(mR, mOrientation);
             float azimuthInRadians = mOrientation[0];
             float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+            compassAngle.setText("Azimuth: " + ((int) Math.round(azimuthInDegress)+"°"));
+
+
             RotateAnimation ra = new RotateAnimation(
                     mCurrentDegree,
                     -azimuthInDegress,
@@ -237,25 +202,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private void getCompass(SensorEvent event) {
-        //float degree = Math.round(event);
-
-        float xv = Math.round(event.values[0]);
-        float yv = Math.round(event.values[1]);
-        float zv = Math.round(event.values[2]);
-        float degree = Math.round(xv);
-
-        compassAngle.setText("Ausrichtung X: " + Float.toString(xv) + " Y:" + Float.toString(yv) + " Z: " + Float.toString(zv));
-
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(mCurrentDegree, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        // how long the animation will take place
-        ra.setDuration(210);
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
-        // Start the animation
-        mPointer.startAnimation(ra);
-        mCurrentDegree = -degree;
-    }
 
 }
